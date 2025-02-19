889ffcf6302a79a74bde306e58008c99ed28dae8
MarkTech32 <marktech370@gmail.com>
2025-02-19 09:31:41 -0600

    Semana 5 Finalizando el CRUD de Categorias

--- src/main/java/com/tienda/service/CategoriaService.java
+++ src/main/java/com/tienda/service/CategoriaService.java
@@ -13,12 +13,38 @@ public class CategoriaService {
     @Autowired
     private CategoriaRepository categoriaRepository;
     
+    
     @Transactional(readOnly=true)
     public List<Categoria> getCategorias(boolean activos){
-         
         var lista = categoriaRepository.findAll();
         
+        
         return lista;
+    }
+    
+    //Metodo de Get
+    @Transactional(readOnly=true)
+    public Categoria getCategoria(Categoria categoria){
+        categoria = categoriaRepository.findById(categoria.getIdCategoria()).orElse(null);
+        
+        return categoria;
+    }
+    
+    //Metodos de Delete
+    @Transactional
+    public void delete(Categoria categoria){
+        //Si el idCategoria pasado existe, se elimina ese registro
+         categoriaRepository.delete(categoria);
+        
+    }
+    
+    //Metodos de Save
+    @Transactional
+    public void save(Categoria categoria){
+        //Si el objeto categoria tiene info en idCategoria, se actualiza el registro
+        //Si el objeto categoria NO tiene info en idCategoria, se inserta un nuevo registro
+         categoriaRepository.save(categoria);
+        
     }
     
 }
