0340cf1ccee3039412973874166f6285f4504037
MarkTech32 <marktech370@gmail.com>
2025-04-13 00:05:46 -0600

    Semana 13 Carrito 2 de 2

--- src/main/java/com/tienda/controller/CarritoController.java
+++ src/main/java/com/tienda/controller/CarritoController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -45,5 +46,33 @@ public class CarritoController {
         
         return new ModelAndView("/carrito/fragmentos :: verCarrito");
     }
+    
+     @GetMapping("/listado")
+     public String listado (Model model){
+         var lista = itemService.getItems();
+         var totalCompra = itemService.getTotal();
+         model.addAttribute("listaItems", lista);
+         model.addAttribute("totalCompra", totalCompra);
+         return "/carrito/listado";
+     }
+     
+     @GetMapping("/eliminar/{idProducto}")
+     public String eliminar (Model model, Item item){
+         itemService.delete(item);
+         return "redirect:/carrito/listado";
+     }
+     
+     @GetMapping("/modificar/{idProducto}")
+     public String modificar (Model model, Item item){
+         item = itemService.getItem(item);
+         model.addAttribute("item", item);
+         return "/carrito/modifica";
+     }
+     
+     @PostMapping("/guardar")
+     public String guardar (Item item){
+         itemService.update(item);
+         return "redirect:/carrito/listado";
+     }
     
 }
